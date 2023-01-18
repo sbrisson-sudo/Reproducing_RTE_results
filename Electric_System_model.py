fe20debde24c8db329e59f8cb542a34be03647f7
sbrisson-sudo <sylvain.brisson@ens.fr>
2023-01-18 11:19:07 +0100

    add quentin H2 PtGtP

--- Electric_System_model.py
+++ Electric_System_model.py
@@ -34,7 +34,6 @@ def GetElectricitySystemModel(Parameters):
     model = set_Operation_Constraints_stockCtr(model)
     model = set_Operation_Constraints_Ramp(model)
     model = set_Operation_Constraints_flex(model)
-    
     ########################
     # Planning constraints #
     ########################
@@ -44,6 +43,8 @@ def GetElectricitySystemModel(Parameters):
     ##########################
     # Additional constraints #
     ##########################
+
+    #STORAGE POWER_CAPACITY_BINDING
     def storage_mw_mwh_binding(model):
         Set_names = get_allSetsnames(model)
         my_set_names = list(Set_names)
@@ -69,7 +70,77 @@ def GetElectricitySystemModel(Parameters):
                                                                  rule=Storage_Power_Capacity_binding_rule)
         return model
 
+    #H2 PRODUCTION FOR STORAGE
+    def set_h2_storage_Ctr(model):
+        Set_names = get_allSetsnames(model)
+        my_set_names = list(Set_names)
+        if "AREAS" in my_set_names:
+            if "CCG - H2" in list(model.TECHNOLOGIES) and "TAC - H2" in list(model.TECHNOLOGIES):
+                model.h2_storage = Var(model.AREAS, model.Date, domain=NonNegativeReals)
+                def h2_storage_rule(model,area):
+                    return sum(model.h2_storage[area,t] for t in model.Date) == sum(model.energy[area,t, "CCG - H2"]/(0.6*0.55) + model.energy[area,t, "TAC - H2"]/(0.4*0.55) for t in model.Date)
+                model.h2_storageCtr=Constraint(model.AREAS,rule=h2_storage_rule)
+
+
+        else:
+            if "CCG - H2" in list(model.TECHNOLOGIES) and "TAC - H2" in list(model.TECHNOLOGIES):
+                model.h2_storage = Var(model.Date, domain=NonNegativeReals)
+
+                def h2_storage_rule(model):
+                    return sum(model.h2_storage[t] for t in model.Date) == sum(
+                        model.energy[t, "CCG - H2"]/(0.6*0.55) + model.energy[t, "TAC - H2"]/(0.4*0.55) for t in model.Date)
+
+                model.h2_storageCtr = Constraint(rule=h2_storage_rule)
+
+        return model
+
+    def set_Operation_Constraints_consum_flex_Ctr(model): #existing constraints overwritten
+        Set_names = get_allSetsnames(model)
+        my_set_names = list(Set_names)
+        model.del_component("consum_flex_Ctr")
+        model.del_component("consum_flex_Ctr_index")
+        if "CCG - H2" in list(model.TECHNOLOGIES) and "TAC - H2" in list(model.TECHNOLOGIES):
+            if "AREAS" in my_set_names:
+                def consum_flex_rule(model, area, t, conso_type):
+                    if conso_type=="H2":
+                        return model.flex_consumption[area, t, conso_type] == model.to_flex_consumption[area, t, conso_type]*(
+                            1 - model.flex[area, t, conso_type])+ model.h2_storage[area,t]
+                    else:
+                        return model.flex_consumption[area, t, conso_type] == model.to_flex_consumption[area, t, conso_type] * (
+                            1 - model.flex[area, t, conso_type])
+
+                model.consum_flex_Ctr = Constraint(model.AREAS, model.Date, model.FLEX_CONSUM, rule=consum_flex_rule)
+            else:
+                def consum_flex_rule(model, t, conso_type):
+                    if conso_type == "H2":
+                        return model.flex_consumption[t, conso_type] == model.to_flex_consumption[
+                            t, conso_type] * (
+                                       1 - model.flex[t, conso_type]) + model.h2_storage[t]
+                    else:
+                        return model.flex_consumption[t, conso_type] == model.to_flex_consumption[t, conso_type] * (
+                                1 - model.flex[t, conso_type])
+
+                model.consum_flex_Ctr = Constraint(model.Date, model.FLEX_CONSUM, rule=consum_flex_rule)
+        else:
+            if 'AREAS' in Set_names:
+                def consum_flex_rule(model, area, t, conso_type):
+                    return model.flex_consumption[area, t, conso_type] == model.to_flex_consumption[
+                        area, t, conso_type] * (
+                                   1 - model.flex[area, t, conso_type])
+
+                model.consum_flex_Ctr = Constraint(model.AREAS, model.Date, model.FLEX_CONSUM, rule=consum_flex_rule)
+            else:
+                def consum_flex_rule(model, t, conso_type):
+                    return model.flex_consumption[t, conso_type] == model.to_flex_consumption[t, conso_type] * (
+                            1 - model.flex[t, conso_type])
+
+                model.consum_flex_Ctr = Constraint(model.Date, model.FLEX_CONSUM, rule=consum_flex_rule)
+        return model
+
+
     model = storage_mw_mwh_binding(model)
+    model = set_h2_storage_Ctr(model)
+    model= set_Operation_Constraints_consum_flex_Ctr(model)
 
 
     return model
